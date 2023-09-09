ce630e2b3d2d1b5a48462a21e18a131197221e48
Guy Fergusson <thatguy.devs@gmail.com>
2023-09-09 18:03:37 +0800

    added spark.ui, move components to base folder, updated index

---
+++ src/BlazorWebAssembly/Views/Bases/Variants.cs
@@ -0,0 +1,31 @@
+﻿namespace Spark.UI.Components;
+
+public enum AlertVariant
+{
+	Default,
+	Success,
+	Info,
+	Warning,
+	Destructive,
+}
+
+public enum BadgeVariant
+{
+	Default,
+	Secondary,
+	Outline,
+	Success,
+	Info,
+	Warning,
+	Destructive,
+}
+
+public enum ButtonVariant
+{
+    Default,
+    Destructive,
+    Outline,
+    Secondary,
+    Ghost,
+    Link
+}
