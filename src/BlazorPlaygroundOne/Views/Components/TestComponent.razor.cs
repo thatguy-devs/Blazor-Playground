dd40a39a8a80476c3794a215df0aea4ca23a9d0f
Guy Fergusson <thatguy.devs@gmail.com>
2024-05-05 23:19:51 +0800

    updated login event, extra pages, update base component and use on test component

---
+++ src/BlazorPlaygroundOne/Views/Components/TestComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿using BlazorPlaygroundOne.Models.ContainerComponent;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorPlaygroundOne.Views.Components
+{
+	public partial class TestComponent : ComponentBase
+	{
+		public ComponentState State { get; set; }
+		public string ErrorMessage { get; set; }
+
+		protected async override Task OnInitializedAsync()
+		{
+			try
+			{
+                this.State = ComponentState.Loading;
+            }
+			catch (Exception exception)
+			{
+				this.ErrorMessage = exception.Message;
+				this.State = ComponentState.Error;
+			}
+		}
+
+        protected async override Task OnParametersSetAsync()
+        {
+			// simulate loading data here.
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            this.State = ComponentState.Content;
+        }
+    }
+}
